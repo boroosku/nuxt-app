bcf5da4f2d1edd8c8e9b21b44156a6e856d4fa3a
boroosku <boroosku@gmail.com>
2024-03-04 00:35:53 +1000

    first task

---
+++ app.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+console.log("Hello world!")
+</script>
+
+<template>
+  <div>
+    <h2 class="montserrat">Hello world!</h2>
+    <h2 class="open-sans">Hello world!</h2>
+  </div>
+</template>
+
+<style scoped>
+.open-sans {
+  font-family: "Open Sans", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: italic;
+  font-variation-settings:
+    "wdth" 100;
+}
+
+.montserrat {
+  font-family: "Montserrat", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
+}
+</style>
